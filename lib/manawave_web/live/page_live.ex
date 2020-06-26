a046e5ca3678d395bb021677768d88727150066e
okkdev <stehlik.jen@gmail.com>
2020-06-26 16:10:15 +0200

    landing page

--- lib/manawave_web/live/page_live.ex
+++ lib/manawave_web/live/page_live.ex
@@ -5,4 +5,14 @@ defmodule ManawaveWeb.PageLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  @impl true
+  def handle_event("submit", %{"table" => ""}, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("submit", %{"floor" => floor, "table" => table}, socket) do
+    {:noreply, push_redirect(socket, to: Routes.live_path(socket, ManawaveWeb.WaveLive, floor, table))}
+  end
 end
