21af3f2784f941049fd54a14992419fd39bb870f
okkdev <stehlik.jen@gmail.com>
2020-06-26 18:11:35 +0200

    check origin setting

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :manawave, ManawaveWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["manawave.gigalixirapp.com", "*.manabar.ch"]
 
 # Do not print debug messages in production
 config :logger, level: :info
