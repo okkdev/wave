caab48a693b9c02873750514d59f69a5abd78fdd
okkdev <stehlik.jen@gmail.com>
2020-07-11 15:06:56 +0200

    add manabar colors

---
+++ assets/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "manabar-dark": "#343434",
+        "manabar-blue": "#273582",
+        "manabar-gray": "#595959",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
