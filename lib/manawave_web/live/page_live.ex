a982535bb8d83bc7956bcbdcf34e3d2f87606cf8
okkdev <stehlik.jen@gmail.com>
2020-07-11 16:00:55 +0200

    🎨table number dropdown

--- lib/manawave_web/live/page_live.ex
+++ lib/manawave_web/live/page_live.ex
@@ -3,19 +3,28 @@ defmodule ManawaveWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, set_table_number(socket, "EG")}
   end
 
   @impl true
-  def handle_event("submit", %{"table" => ""}, socket) do
-    socket =
-      socket
-      |> put_flash(:error, "Bitte Tischnummer angeben!")
-    {:noreply, socket}
+  def handle_event("floor", %{"floor" => floor}, socket) do
+    {:noreply, set_table_number(socket, floor)}
   end
 
   @impl true
   def handle_event("submit", %{"floor" => floor, "table" => table}, socket) do
     {:noreply, push_redirect(socket, to: Routes.live_path(socket, ManawaveWeb.WaveLive, floor, table))}
+  end
+
+  defp set_table_number(socket, "EG") do
+    assign(socket, table_num: Enum.to_list(1..6))
+  end
+
+  defp set_table_number(socket, "OG") do
+    assign(socket, table_num: Enum.to_list(1..7))
+  end
+
+  defp set_table_number(socket, "UG") do
+    assign(socket, table_num: Enum.to_list(1..2))
   end
 end
