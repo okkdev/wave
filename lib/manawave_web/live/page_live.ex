f3391a52ef6df53ed764f019221a7315c716eb9a
okkdev <stehlik.jen@gmail.com>
2020-07-15 20:58:37 +0200

    🐛fixed table selection

--- lib/manawave_web/live/page_live.ex
+++ lib/manawave_web/live/page_live.ex
@@ -6,14 +6,16 @@ defmodule ManawaveWeb.PageLive do
     socket =
       socket
       |> assign(:floors, ["-", "EG", "OG", "UG", "AT"])
-      |> assign_tables("-")
+      |> assign(:current_floor, "-")
+      |> assign(:current_table, "-")
+      |> assign_tables()
 
     {:ok, socket}
   end
 
   @impl true
-  def handle_event("floor", %{"picker" => %{"floor" => floor}}, socket) do
-    {:noreply, assign_tables(socket, floor)}
+  def handle_event("update", %{"picker" => %{"floor" => floor, "table" => table}}, socket) do
+    {:noreply, assign_tables(socket, floor, table)}
   end
 
   @impl true
@@ -22,11 +24,23 @@ defmodule ManawaveWeb.PageLive do
      push_redirect(socket, to: Routes.live_path(socket, ManawaveWeb.WaveLive, floor, table))}
   end
 
-  defp assign_tables(socket, floor) do
+  defp assign_tables(socket, floor \\ "-", table \\ "-") do
     socket
-    |> toggle_disable(floor)
+    |> assign_current_table(floor, table)
     |> set_table_amount(floor)
-    |> assign(:current_floor, floor)
+  end
+
+  defp assign_current_table(socket, floor, table) do
+    if socket.assigns.current_floor === floor do
+      socket
+      |> assign(:current_table, table)
+      |> toggle_disable(floor, table)
+    else
+      socket
+      |> assign(:current_floor, floor)
+      |> assign(:current_table, "-")
+      |> toggle_disable(floor, "-")
+    end
   end
 
   defp set_table_amount(socket, "-") do
@@ -34,26 +48,34 @@ defmodule ManawaveWeb.PageLive do
   end
 
   defp set_table_amount(socket, "EG") do
-    assign(socket, tables: Enum.to_list(1..6))
+    assign_table_amount(socket, 1..6)
   end
 
   defp set_table_amount(socket, "OG") do
-    assign(socket, tables: Enum.to_list(1..7))
+    assign_table_amount(socket, 1..7)
   end
 
   defp set_table_amount(socket, "UG") do
-    assign(socket, tables: Enum.to_list(1..2))
+    assign_table_amount(socket, 1..2)
   end
 
   defp set_table_amount(socket, "AT") do
-    assign(socket, tables: Enum.to_list(1..2))
+    assign_table_amount(socket, 1..2)
   end
 
-  defp toggle_disable(socket, "-") do
+  defp assign_table_amount(socket, tables) do
+    assign(socket, tables: ["-" | Enum.to_list(tables)])
+  end
+
+  defp toggle_disable(socket, "-", _) do
     assign(socket, :disabled, true)
   end
 
-  defp toggle_disable(socket, _) do
+  defp toggle_disable(socket, _, table) when table !== "-" do
     assign(socket, :disabled, false)
+  end
+
+  defp toggle_disable(socket, _, _) do
+    assign(socket, :disabled, true)
   end
 end
