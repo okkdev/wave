a075d8cdb0fa3af1d643cf5b9c514a9ff0185d9f
okkdev <stehlik.jen@gmail.com>
2020-10-28 11:31:08 +0100

    💅 tailwind

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Wave.MixProject do
   def application do
     [
       mod: {Wave.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
