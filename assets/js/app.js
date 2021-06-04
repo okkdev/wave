725e220a70ab5c93f03f24df29a027d6883ecf0e
okkdev <stehlik.jen@gmail.com>
2021-06-04 13:16:08 +0200

    🐛 fix ding

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,15 @@
 // its own CSS file.
 import "../css/app.scss"
 
+const ding = new Audio("/sounds/ding.mp3")
+
+let Hooks = {}
+Hooks.Ding = {
+  mounted() {
+    ding.play()
+  },
+}
+
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
@@ -13,16 +22,21 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
-import {Socket} from "phoenix"
+import { Socket } from "phoenix"
 import NProgress from "nprogress"
-import {LiveSocket} from "phoenix_live_view"
+import { LiveSocket } from "phoenix_live_view"
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content")
+let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
+  params: { _csrf_token: csrfToken },
+})
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+window.addEventListener("phx:page-loading-start", (info) => NProgress.start())
+window.addEventListener("phx:page-loading-stop", (info) => NProgress.done())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -32,4 +46,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
