7f571e7d04cb4c86a1417a0ba04821f342a2b338
okkdev <stehlik.jen@gmail.com>
2021-06-04 23:24:55 +0200

    🔥

--- lib/wave_web/controllers/contact_controller.ex
+++ lib/wave_web/controllers/contact_controller.ex
@@ -24,38 +24,4 @@ defmodule WaveWeb.ContactController do
         render(conn, "index.html", changeset: changeset)
     end
   end
-
-  # @impl true
-  # def mount(_params, _session, socket) do
-  #   contact_changeset = Tracing.change_contact(%Contact{})
-  #   {:ok, assign(socket, changeset: contact_changeset)}
-  # end
-
-  # @impl true
-  # def handle_event("validate", %{"contact" => contact_params}, socket) do
-  #   changeset =
-  #     %Contact{}
-  #     |> Tracing.change_contact(contact_params)
-  #     |> Map.put(:action, :validate)
-
-  #   {:noreply, assign(socket, :changeset, changeset)}
-  # end
-
-  # @impl true
-  # def handle_event("submit", %{"contact" => contact_params}, socket) do
-  #   case Tracing.create_contact(contact_params) do
-  #     {:ok, _contact} ->
-  #       Pubsub.broadcast(:trace)
-
-  #       {
-  #         :noreply,
-  #         socket
-  #         |> put_flash(:info, "Submitted successfully")
-  #         #  |> push_redirect(to: socket.assigns.return_to)
-  #       }
-
-  #     {:error, %Ecto.Changeset{} = changeset} ->
-  #       {:noreply, assign(socket, changeset: changeset)}
-  #   end
-  # end
 end
